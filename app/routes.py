39b6a5c5d4cd59d8a7e159339e33c676900a801a
Jared Guevara <jaredpaulgue@berkeley.edu>
2024-07-18 11:53:14 -0700

    Finish prototype of Build-A-Model w/ drag-n-drop and RMSE calc

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template, url_for, request, jsonify
 import pandas as pd
 import json
 import plotly as pl
 import plotly_express as px
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+train = pd.read_csv('./app/data/processed_data/ccao_train_cleaned.csv').drop('Unnamed: 0', axis=1)
 
 @app.route('/')
 def index():
@@ -11,9 +16,25 @@ def index():
 
 @app.route('/build-a-model')
 def model1():
-    train = pd.read_csv('./app/data/processed_data/ccao_train_cleaned.csv').drop('Unnamed: 0', axis=1)
     data_preview = train.head().to_html(classes='table table-striped table-bordered', index=False)
-    return render_template('model1.html', title = 'Build-A-Model', data = data_preview)
+    return render_template('model1.html', title = 'Build-A-Model', data = data_preview, data_columns = train.columns.to_list())
+
+@app.route('/train', methods=['POST'])
+def train_model():
+    data = request.json
+    selected_features = data['features']
+
+    X = train[selected_features]
+    y = train['Sale Price']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    preds = model.predict(X_test)
+    rmse = mean_squared_error(y_test, preds, squared=False)
+ 
+    return jsonify({'rmse' : rmse})
 
 @app.route('/spam-vs-ham')
 def model2():
